16093fac14c8298460c1cc06df958c365d09a5e7
zero <tongjizero2011@163.com>
2013-11-11 22:39:46 +0800

    新闻公告的Model类

---
+++ TaxiFirm/TaxiFirm/Models/News/News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaxiFirm.Models;
+namespace TaxiFirm.Models.News
+{
+    public class News
+    {
+        public int NewsId { get; set; }
+        public string Title { get; set; }
+        public int employee_id { get; set; }
+        public DateTime publish_time { get; set; }
+        public string content { get; set; }
+        public string picture_path { get; set; }
+        public string author { get; set; }
+        public News(){}
+    }
+}
